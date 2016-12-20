a549eeab12d0be5198009592cd9e4ac440f8bc0a
manudevelopia <manugarciarodriguez@gmail.com>
2016-12-20 14:11:32 +0100

    Added POST method and body parserdep

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, function (){
   console.log('Listening on port 3000');
@@ -7,4 +11,8 @@ app.listen(3000, function (){
 
 app.get('/', function (req, res) {
   res.sendFile('/home/manu/Developer/Github/node-crud-test/www/index.html');
+});
+
+app.post('/', (req, res) => {
+ console.log(req.body); 
 });
