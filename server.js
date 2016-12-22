894522cd2be43e7a2cb7df0cc2395b929e3cf9c9
manudevelopia <manugarciarodriguez@gmail.com>
2016-12-22 07:11:12 +0100

    Add landing json. clean and formating

--- server.js
+++ server.js
@@ -9,15 +9,6 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.sendFile('/home/manu/Developer/Github/node-crud-test/www/index.html');
-  var cursor = db.collection('projects').find();
-});
-
-app.post('/projects', (req, res) => {
-  console.log(req.body); 
-});
-
 mongoClient.connect('mongodb://localhost:27017/local', (err, database) => {
   if (err){return console.log(err);}
 
@@ -25,4 +16,16 @@ mongoClient.connect('mongodb://localhost:27017/local', (err, database) => {
   app.listen(3000, function (){
     console.log('Listening on port 3000');
   }); 
+});
+
+app.get('/', function (req, res) {
+  res.send({
+    app: 'node-test', 
+    ver: '0.1.0',
+    author: '@manudevelopia'
+  });
+});
+
+app.post('/projects', (req, res) => {
+  console.log(req.body); 
 });
