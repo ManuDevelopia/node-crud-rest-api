8c197c9503ee9db92e79bdf75015b41eb7ae009a
manudevelopia <manugarciarodriguez@gmail.com>
2016-12-22 22:48:55 +0100

    Create and show elements from database

--- server.js
+++ server.js
@@ -19,13 +19,29 @@ mongoClient.connect('mongodb://localhost:27017/local', (err, database) => {
 });
 
 app.get('/', function (req, res) {
+  var cursor = db.collection('projects').find()
+    .toArray(function (err, results){
+      console.log(results);
+  });
+  
   res.send({
     app: 'node-test', 
     ver: '0.1.0',
     author: '@manudevelopia'
   });
+
+});
+
+app.get('/projects', function (req, res){
+  res.sendFile('/home/manu/Developer/Github/node-crud-test/www/index.html');
 });
 
 app.post('/projects', (req, res) => {
-  console.log(req.body); 
+  db.collection('projects').save(req.body, (err, result) =>{
+    if (err) return console.log(err);
+  });
+
+  console.log('Saved in database');
+  res.redirect('/');
+
 });
