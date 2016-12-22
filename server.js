3449ccfd8339205fc4e826afae3e699de0e6ccf7
manudevelopia <manugarciarodriguez@gmail.com>
2016-12-22 06:50:30 +0100

    MongoDB connection has been added

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const mongoClient = require('mongodb').MongoClient;
+var db;
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.listen(3000, function (){
-  console.log('Listening on port 3000');
-});
 
 app.get('/', function (req, res) {
   res.sendFile('/home/manu/Developer/Github/node-crud-test/www/index.html');
 });
 
 app.post('/', (req, res) => {
- console.log(req.body); 
+  console.log(req.body); 
+});
+
+mongoClient.connect('mongodb://localhost:27017/local', (err, database) => {
+  if (err){return console.log(err);}
+
+  db = database;
+  app.listen(3000, function (){
+    console.log('Listening on port 3000');
+  }); 
 });
