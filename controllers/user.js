4c7575221b44daf68758505ce868e007aa44c1f1
manudevelopia <manugarciarodriguez@gmail.com>
2016-12-31 00:22:31 +0100

    Implement controller fro User Scheme

---
+++ controllers/user.js
@@ -0,0 +1,32 @@
+var User = require('../models/user');
+
+exports.findAll = function(req, res){
+  User.find(function(req, users){
+    res.status(200).jsonp(users);
+  });
+};
+
+// GET Project by ID
+exports.findById = function(req, res){
+  User.findById(req.params.id, function(err, user){
+    if (err){
+      return res.send(500, err.message);
+    }
+  
+    res.status(200).jsonp(user);
+  });
+};
+
+exports.create = function(req, res){
+  var user = new User({
+    name: req.body.name
+  });
+
+  user.save(function(err, user){
+    if (err){
+      return res.send(500, err.message);
+    }
+
+    res.status(200).jsonp(user);
+  });
+};
